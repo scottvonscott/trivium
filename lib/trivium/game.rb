841e88d7de8d48c40a9cab2db551ce7fe13c7393
scottvonscott <skeplin17@gmail.com>
2020-05-28 16:25:49 -0700

    added missed_question method

--- lib/trivium/game.rb
+++ lib/trivium/game.rb
@@ -1,52 +1,66 @@
 class Game
 
-    attr_accessor :score, :turn, :difficulty
+    attr_accessor :score, :turn, :difficulty, :missed_questions
 
     def initialize(difficulty)
-        puts "\nA new game has started!"
-        puts "\nYou're playing on #{difficulty}? Ok... if you say so."
-        puts "Press enter to continue"
-        gets.strip
         @score = 0
         @turn = 1
+        @missed_questions = []
         API.get_trivia(difficulty)
-        self.play(Question.all)
+        
+        puts "\nA new game has started!"
+        puts "\nYou're playing on #{difficulty}? Ok... if you say so."
+            puts "Press enter to continue"
+            gets.strip
+            play(Question.all)
     end
 
-def score
-    @score
-end
+    def score
+        @score
+    end
 
-def turn
-    @turn
-end
+    def turn
+        @turn
+    end
 
-def results
-    puts "\nYour score is #{@score} out of #{Question.all.size}"
-    puts "\nWould you like to play again?"
-    user_input = gets.strip.downcase
-        if user_input == "yes" || user_input == "y"
-            puts "TRIVIA IS LIFE"
-            Question.destroy_all
-            CLI.new.start
-        else
-            puts "OK, fine then...."
+    def missed_questions
+        @missed_questions.each.with_index do |question|
+            puts "#{index}. #{question}"
         end
-end
-
-def randomize_answers(correct, incorrect)
-    randomized = []
-    randomized << correct
-    incorrect.each do |answer|
-        randomized << answer
     end
-    randomized.sample(4).each.with_index(1) do |answers, index|
-        puts "#{index}. #{answers}"
-    end 
-end
+
+    def results
+        puts "\nYour score is #{@score} out of #{Question.all.size}"
+        puts "\nWould you like to see questions you answered incorrectly? Seems like a good chance to learn more!"
+    
+        start_over
+    end
+
+    def start_over
+        puts "\nWould you like to play again?"
+        user_input = gets.strip.downcase
+            if user_input == "yes" || user_input == "y"
+                puts "TRIVIA IS LIFE"
+                Question.destroy_all
+                CLI.new.start
+            else
+                puts "OK, fine then...."
+            end
+    end
+
+    def randomize_answers(correct, incorrect)
+        randomized = []
+        randomized << correct
+        incorrect.each do |answer|
+            randomized << answer
+            end
+        randomized.sample(4).each.with_index(1) do |answers, index|
+            puts "#{index}. #{answers}"
+            end 
+    end
 
 
-def play (questions)
+    def play (questions)
     questions.each do |question|
         puts "-------------------------------------------------------------------------------"
         puts  "\nQuestion #{@turn}..."
@@ -67,9 +81,11 @@ def play (questions)
 
             
             else 
+                @missed_questions << question.text
                 puts "\nINCORRECT!"
-                puts "The correct answer was #{question.correct_answer}"
-                puts "Press enter to continue"
+                puts "\nThe correct answer was #{question.correct_answer}"
+                puts "\nMaybe you're not great with #{question.category}..."
+                puts "\nPress enter to continue"
                 gets.strip
                 puts "-------------------------------------------------------------------------------"
 
